d65b4ca1833cca553dfbfe58c7dd01de2485bab8
Karuppasamy <karuppasamy.m@cognizant.com>
2012-11-30 15:45:32 +0530

    added functionality for save the location details

--- controllers/associates_controller.rb
+++ controllers/associates_controller.rb
@@ -4,6 +4,19 @@ class App < Sinatra::Base
    "welocme"
   end
 
+  post '/associates/location' do
+    access_token = params[:access_token]
+    user = params[:user]
+
+    access_token_decode = Base64.decode64(access_token)    
+    user_id, secure_key = access_token.split(" ")    
+
+    location = Location.create(:country => user[:country], :state => user[:state], :city => user[:city])
+    associate_location = AssociateLocation.create(:associate_id => user_id, :location_id => location.id, :vnet => user[:vnet], :seat_number => user[:seat_number], :start_date => user[:start_date], :end_date => user[:end_date])
+    
+    {:access_token => access_token, :status => "success"}.to_json
+  end
+  
   post '/associates' do		
     unless params[:username] == ""
       associate = Associate.new
