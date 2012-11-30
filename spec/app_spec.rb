744d3886e9826af19136c6b01bd391110d239470
rajalakshmi <Rajalakshmi.Velu@cognizant.com>
2012-11-30 19:41:27 +0530

    added spec for update location

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,4 +12,20 @@ end
 			last_response.should be_ok
 		end
 	end
+  
+  describe 'signin' do
+    it 'should login' do
+      post '/users/sign_in',{:user=>"MzQxMjY5IHNhcmFueWEx\n"}
+      last_response.should be_ok
+    end
+  end
+  
+  describe 'Associate can update the location' do
+    it 'Create the location' do
+      params = {:user=>{:country=>"india",:state=>"tamilnadu",:city=>"chennai",:associate_id=>"341269",:vnet=>"451234",:seat_number=>"PKN7A053",:state_date=>Time.now,:end_date=>"Time.now"},:access_token=>"MzQxMjY5IHNhcmFueWEx\n"}
+      post '/associates/location', params
+      last_response.should be_ok
+    end
+  end  
 end
+
